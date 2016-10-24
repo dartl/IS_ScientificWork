f3fc50e4dfa92107397d3e8aee76ad799131ecb7
Kotlin1470 <enotpoloskyn1470@gmail.com>
2016-10-24 19:59:59 +0400

    Добавлены коллекции

--- scientificWork/admin.py
+++ scientificWork/admin.py
@@ -5,10 +5,12 @@ from django.contrib import admin
 # Подключаем наши модели в админку сайта
 
 # Указываем, что из scientificWork.models импортируем конкретную модель Test_collection
-from scientificWork.models import Test_collection
-
+from scientificWork.models import sw_publications, sw_participations, sw_RandD, Test_collection
 # Регистрируем импортированную модель в админку
 admin.site.register(Test_collection)
+admin.site.register(sw_publications)
+admin.site.register(sw_RandD)
+admin.site.register(sw_participations)
 
 
 # Create your models here.
--- scientificWork/models.py
+++ scientificWork/models.py
@@ -9,5 +9,34 @@ from django.db import models
 class Test_collection(models.Model):
     text = models.TextField()
 
+class sw_publications(models.Model):
+ #   _id = models.IntegerField()  #  Идентификатор публикации
+    typePublication = models.TextField("Тип публикации")  # тип публикации
+    publishingHouseName = models.TextField("Название издательства")  # название издательства
+    place = models.TextField("Место издания")  #  место издания
+    date = models.DateField("Дата издания")  #  дата издания
+    volume = models.IntegerField("Объем")  #  объем
+    unitVolume = models.TextField("Единицы объёма")  #  еденицы объема
+    edition = models.IntegerField("Тираж")  #  тираж
+    user = models.IntegerField("Идентификатор пользователя")  #  идентификатор пользователя
 
+    type = models.TextField("Вид методического указания")  #  вид методического издания / книги
+    isbn = models.TextField("ISBN")  #  ISBN
+    number = models.IntegerField("Номер издания")  #  номер издания
+    editor = models.TextField("Редактор сборника")  #  редакто сборника
+    reiteration = models.TextField("Вид повторения сборника")  #  вид повторения сборника
+
+class sw_participations(models.Model):
+ #   _id = models.IntegerField()  # идентифиатор
+    type = models.TextField("Тип (конференция - conference| семинар - seminar)")  #  тип: конференция - conference, семинар - seminar
+    name = models.TextField("Название")  # название
+    date = models.DateField("Дата проведения")  # дата проведения
+    place = models.TextField("Место проведения")  # дата проведения
+    reiteration = models.TextField("Вид проведения (одноразовый - disposable| повторяющийся - repeating")  # вид проведения: одноразовый - disposable, повторяющийся - repeating
+    rank = models.TextField("Ранг") # ранг
+
+class sw_RandD(models.Model):
+  #  id = models.TextField() # идентификатор
+    name = models.TextField("Название НИОКР")  # Название НИОКР
+    cipher = models.TextField("Шифр")  #Шифр
 # Create your models here.
