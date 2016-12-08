1c7838363f16d4dfa2dde7b4ee7b5971d56c22ca
dartl <twisan@mail.ru>
2016-12-08 16:32:00 +0300

    Добавлен набор Юнит-тестов
    Добавлены юнит-тесты для проверки получения страницами данных и
    фильтрация публикации, конкурсов и семинаров и НИОКР. Численность
    аспирантов, докторантов и т.д. так же проверяется.

--- scientificWork/tests.py
+++ scientificWork/tests.py
@@ -9,6 +9,7 @@ from django.test import TestCase
 from scientificWork.models import *
 from django.test.client import Client
 from django.contrib.auth.models import User
+from scientificWork.models import Publication, UserProfile, Rand, Participation
 
 c = Client()
 
@@ -76,3 +77,75 @@ class GetPagesTest(TestCase):
     def test_rads_page(self):
         response = c.get('/scientificWork/rads')
         self.assertEqual(response.status_code, 200)
+
+# Тесты на фильтрацию
+class FilterPagesTest(TestCase):
+	fixtures = ['data.json']
+
+# Публикации:
+	# тест на получение всех публикации
+	def test_publication_all(self):
+		response = c.get('/scientificWork/publications')
+		self.assertEqual(response.context['notes'].__len__(), 3)
+
+	# тест на получение публикаций с типом Книга
+	def test_publication_onlyBook(self):
+		response = c.get('/scientificWork/publications?userName=&typePublication=book&publishingHouseName=&place=&date=&volume=&unitVolume=&edition=&bookName=&type=&isbn=&number=&editor=&nameSbornik=&reiteration=&button_filter=')
+		self.assertEqual(response.context['notes'].__len__(), 1)
+
+	# тест на получение публикаций с типом Статья в журнале
+	def test_publication_onlyStiteInJournal(self):
+		response = c.get('/scientificWork/publications?userName=&typePublication=journal&publishingHouseName=&place=&date=&volume=&unitVolume=&edition=&bookName=&type=&isbn=&number=&editor=&nameSbornik=&reiteration=&button_filter=')
+		self.assertEqual(response.context['notes'].__len__(), 0)
+
+# Конкурсы и семинары
+	# тест на получение всех конкурсов и семинаров
+	def test_competitions_all(self):
+		response = c.get('/scientificWork/competitions')
+		self.assertEqual(response.context['comps'].__len__(), 3)
+
+	# тест на получение повторяющихся конкурсов и семинаров
+	def test_competitions_onlyBook(self):
+		response = c.get('/scientificWork/competitions?userName=&type=&name=&place=&date=&rank=&reiteration=repeating&button_filter=')
+		self.assertEqual(response.context['comps'].__len__(), 3)
+
+	# тест на получение конкурсов и семинаров с названием "авып"
+	def test_competitions_onlyStiteInJournal(self):
+		response = c.get('/scientificWork/competitions?userName=&type=&name=авып&place=&date=&rank=&reiteration=&button_filter=')
+		self.assertEqual(response.context['comps'].__len__(), 0)
+
+# НИОКР
+	# тест на получение всех НИОКР
+	def test_rands_all(self):
+		response = c.get('/scientificWork/rads')
+		self.assertEqual(response.context['rands'].__len__(), 3)
+
+	# тест на получение всех НИОКР на фамилию Столетов
+	def test_rands_onlyStoletov(self):
+		response = c.get('/scientificWork/rads?userName=Столетов&name=&cipher=&button_filter=')
+		self.assertEqual(response.context['rands'].__len__(), 1)
+
+	# тест на получение всех НИОКР на шифр "62784"
+	def test_rands_onlyChihper62784(self):
+		response = c.get('/scientificWork/rads?userName=&name=&cipher=62784&button_filter=')
+		self.assertEqual(response.context['rands'].__len__(), 0)
+
+# Отчёт о численности
+	# количество аспирантов
+	def test_strength_aspirant(self):
+		response = c.get('/scientificWork/strength')
+		self.assertEqual(response.context['aspirant'], 0)
+
+	# количество докторантов
+	def test_strength_doctorant(self):
+		response = c.get('/scientificWork/strength')
+		self.assertEqual(response.context['doctorant'], 2)
+
+	def test_strength_soiskatel(self):
+		response = c.get('/scientificWork/strength')
+		self.assertEqual(response.context['soiskatel'], 2)
+
+	# количество стажеров
+	def test_strength_stajer(self):
+		response = c.get('/scientificWork/strength')
+		self.assertEqual(response.context['stajer'], 0)
